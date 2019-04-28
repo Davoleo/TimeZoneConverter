f8d211dab62a5045af19b12857d2c7faf066be2f
Davoleo <davoleo2001@gmail.com>
2019-04-28 19:54:02 +0200

    More UI Work

--- src/davoleo/timezoneconverter/gui/Main.java
+++ src/davoleo/timezoneconverter/gui/Main.java
@@ -10,11 +10,11 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("gui/main.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
         primaryStage.setTitle("TimeZones Converter");
 
 
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 750, 430));
         primaryStage.show();
     }
 
