c5dc4b3fb3ac1eea00543a97bfbd0a5a5b9595f6
Davoleo <davoleo2001@gmail.com>
2019-05-17 21:29:22 +0200

    Revert v1 to no-fxml

--- src/davoleo/timezoneconverter/gui/Main.java
+++ src/davoleo/timezoneconverter/gui/Main.java
@@ -1,20 +1,39 @@
 package davoleo.timezoneconverter.gui;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
+
+import java.time.LocalDateTime;
 
 public class Main extends Application {
 
+    private Slider slider1, slider2;
+    private TextField time1, time2;
+    private Label zone1, zone2;
+    private Button deltaButton;
+    private CheckBox lockDelta;
+
+
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
-        primaryStage.setTitle("TimeZones Converter");
+        primaryStage.setTitle("Time Zones Converter");
 
+        slider1 = new Slider(0, 24, 12);
+        slider2 = new Slider(0, 24, 12);
 
-        primaryStage.setScene(new Scene(root, 750, 430));
+        time1 = new TextField(LocalDateTime.now().getHour() + " : " + LocalDateTime.now().getMinute());
+        time2 = new TextField(LocalDateTime.now().getHour() + " : " + LocalDateTime.now().getMinute());
+
+        GridPane layout = new GridPane();
+        layout.setHgap(8);
+        layout.setHgap(8);
+        layout.setPadding(new Insets(8));
+
+        primaryStage.setScene(new Scene(layout, 750, 430));
         primaryStage.show();
     }
 
